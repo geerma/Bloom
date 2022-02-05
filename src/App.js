9c89999816ca4215845642517bc8519056402d44
MihirSeth <mihirseth2005@gmail.com>
2022-02-05 10:21:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Form from './components/Form'
+// import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Bloom. </p>
-        <p> Bloom when the world feels like chaos.</p>
-        <Form />
+       
+      <h1> Bloom when the world feels like chaos.</h1>
+      <p> Bloom when the world feels like chaos.</p>
+
       </header>
+
+      <form class='form'>
+            <p className='question'>How was your day?</p>
+            <div className='howWasYourDay'> 
+                <input className='emotion' type = "button"></input>
+                <input className='emotion'type = "button"></input>
+                <input className='emotion'type = "button"></input>
+            </div>
+
+            <p className='question'>Highlights for the Day</p>
+            <div className='highlightsOfYourDay'> 
+                <input type = "text"></input>
+            </div>
+
+            <p className='question'>How could I have made this day better?</p>
+            <div className='makeThisDayBetter'> 
+                <input type = "text"></input>
+            </div>
+
+            <p className='question'>What are you looking foward to tomorrow? </p>
+            <div className='lookingFoward'> 
+                <input type = "text"></input>
+            </div>
+  
+        </form>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,17 @@
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
 const Form = () => {
     return (
         <header>
             <h1>Form Starts Here</h1>
-            <div>
-            <input type = "button"></input>
-            <input type = "button"></input>
-            <input type = "button"></input>
+            <p>How was your day?</p>
+            <div class='howWasYourDay'> 
+                <input class='emotion' type = "button"></input>
+                <input class='emotion'type = "button"></input>
+                <input class='emotion'type = "button"></input>
             </div>
+
+
             <div>
             <input type = "text"></input>
             <input type = "text"></input>
