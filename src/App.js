2c7ebe1f0dc4a656618a4277ddc96131a633df84
MihirSeth <mihirseth2005@gmail.com>
2022-02-05 10:30:37 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-// import Form from './components/Form'
+import Form from './components/Form'
 
 function App() {
   return (
@@ -12,30 +12,7 @@ function App() {
 
       </header>
 
-      <form class='form'>
-            <p className='question'>How was your day?</p>
-            <div className='howWasYourDay'> 
-                <input className='emotion' type = "button"></input>
-                <input className='emotion'type = "button"></input>
-                <input className='emotion'type = "button"></input>
-            </div>
-
-            <p className='question'>Highlights for the Day</p>
-            <div className='highlightsOfYourDay'> 
-                <input type = "text"></input>
-            </div>
-
-            <p className='question'>How could I have made this day better?</p>
-            <div className='makeThisDayBetter'> 
-                <input type = "text"></input>
-            </div>
-
-            <p className='question'>What are you looking foward to tomorrow? </p>
-            <div className='lookingFoward'> 
-                <input type = "text"></input>
-            </div>
-  
-        </form>
+      < Form/>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,32 @@
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 
 const Form = () => {
     return (
-        <header>
-            <h1>Form Starts Here</h1>
-            <p>How was your day?</p>
-            <div class='howWasYourDay'> 
-                <input class='emotion' type = "button"></input>
-                <input class='emotion'type = "button"></input>
-                <input class='emotion'type = "button"></input>
+
+        <form class='form'>
+            <p className='question'>How was your day?</p>
+            <div className='howWasYourDay'> 
+                <input className='emotion' type = "button"></input>
+                <input className='emotion'type = "button"></input>
+                <input className='emotion'type = "button"></input>
             </div>
 
-
-            <div>
-            <input type = "text"></input>
-            <input type = "text"></input>
-            <input type = "text"></input>
+            <p className='question'>Highlights for the Day</p>
+            <div className='highlightsOfYourDay'> 
+                <input type = "text"></input>
             </div>
-            
-        </header>
+
+            <p className='question'>How could I have made this day better?</p>
+            <div className='makeThisDayBetter'> 
+                <input type = "text"></input>
+            </div>
+
+            <p className='question'>What are you looking foward to tomorrow? </p>
+            <div className='lookingFoward'> 
+                <input type = "text"></input>
+            </div>
+
+        </form>
     )
 }
 
