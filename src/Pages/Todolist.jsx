d5ade7dece3c3eee667f2778c767f495d34e8a81
geerma <geerm1997@gmail.com>
2022-02-05 12:25:35 -0700

    Todolist Update

--- src/Pages/Todolist.jsx
+++ src/Pages/Todolist.jsx
@@ -26,9 +26,29 @@ export const Todolist = () => {
         }
     ])
 
+    const deleteTask = (id) => {
+        console.log('Delete Task ID:', id)
+    }
+
     return (
         <div>
-        <Tasks tasks={tasks}/>
+        <Tasks tasks={tasks} onDelete={deleteTask}/>
+
+        <p className='question'>Task</p>
+        <div className='whatTask'> 
+        <input type = "text" placeholder="Task"></input>
+        </div>
+
+        <p className='question'>Which Day?</p>
+        <div className='whatDay'> 
+        <input type = "text" placeholder="Date"></input>
+        </div>
+
+        <p className='question'>What Time?</p>
+        <div className='whatWhat'> 
+        <input type = "text" placeholder="Time"></input>
+        </div>
+
         </div>
     ); 
   };
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,15 @@
 import Task from './Task'
 import { useState } from 'react'
 
-const Tasks = ({tasks}) => {
+const Tasks = ({tasks, onDelete}) => {
 
     return (
         <div>
-            {tasks.map((task) => (
-            <Task key={task.id} task={task} />
-            ))}
+            {tasks.map(
+                (task) => (
+            <Task key={
+                task.id} task={task} onDelete={onDelete} />)
+                )}
         </div>
     )
 }
