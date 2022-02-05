cf3f1f2fa65773b18a56ef527e5657b0ea6773ad
geerma <geerm1997@gmail.com>
2022-02-04 22:23:50 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-// import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form'
 import Tasks from './components/Tasks'
+import {useState} from 'react'
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Task for Tomorrow 1',
+    },
+    {
+        id: 2,
+        text: 'Task for Tomorrow 2',
+    },
+    {
+        id: 3,
+        text: 'Task for Tomorrow 3',
+    }
+])
   return (
     <div className="App">
       <header className="App-header">
