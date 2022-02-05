73966e1dd08686a6a5a6ea496219807c5a653d9a
geerma <geerm1997@gmail.com>
2022-02-04 20:12:01 -0700

    React Update

---
+++ src/components/Form.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+
+const Form = () => {
+    return (
+        <header>
+            <h1>Form Starts Here</h1>
+            <div>
+            <input type = "button"></input>
+            <input type = "button"></input>
+            <input type = "button"></input>
+            </div>
+            <div>
+            <input type = "text"></input>
+            <input type = "text"></input>
+            <input type = "text"></input>
+            </div>
+            
+        </header>
+    )
+}
+
+export default Form
