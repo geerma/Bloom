23e2b308f1f2c3a9af7cdc32afd700e13f38043b
Lily Yao <lilyyao102@gmail.com>
2022-02-04 21:04:47 -0700

    got rid of spinny logo

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to Bloom. </p>
-        <p> Bloom when the world feels like chaos........</p>
+        <p> Bloom when the world feels like chaos</p>
         <Form />
       </header>
 
