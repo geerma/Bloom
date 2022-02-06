c6b66f04ca2fac078f1c40be1c85824e967347c6
MihirSeth <mihirseth2005@gmail.com>
2022-02-06 18:21:18 +0530

    commit

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -73,23 +73,38 @@ const AddTask = ({onAdd}) => {
                         </div>
                     </div>
 
-                    <div className='breaking'>
+                    {/* <div className='breaking'>
+                    <p className='whichDay'>Which day?</p>
+                        <div className='question'> 
+
+                            <div className='allDateContainerToDo'>
+                            <div className='whatDay'>
+                                <form action="/action_page.php">
+                                    <input type="date" id="currentDate" name="currentDate" onChange ={(e) => setDate(e.target.value)}></input>
+                                </form>
+                                </div>           
+                            </div>
+                        </div>
+                    </div> */}
+
+<div className='breaking'>
                     <p className='question'>Which day?</p>
                         <div className='question'> 
-                            <input className='whatDay' type = "text" placeholder="Date" value={date} onChange ={(e) => setDate(e.target.value)}></input>
+                            <input className='whatDay' type = "date" placeholder="Date" value={date} onChange ={(e) => setDate(e.target.value)}></input>
                         </div>
                     </div>
+
 
                     <div className='breaking'>
                         <p className='question'>What Time?</p>
                         <div > 
-                            <input className='whatTime' type = "text" placeholder="Time" value={time} onChange ={(e) => setTime(e.target.value)}></input>
+                            <input className='whatTime' type = "time" placeholder="Time" value={time} onChange ={(e) => setTime(e.target.value)}></input>
                         </div>
                     </div>
                 </div>
 
-                <div className='saveButtonDiv'>
-                    <div><input className="saveTaskButtonContainer" className='saveTaskButton' type = "submit" value ="SAVE TASK" /></div>
+                <div className='saveTaskButtonContainer'>
+                    <div><input className='saveTaskButton' type = "submit" value ="SAVE TASK" /></div>
                 </div>
                 
             </form>
