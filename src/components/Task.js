04596fb1736b0e5d7f6592c52fc72fd79827b10b
atmiyajadvani <atmiya@my.yorku.ca>
2022-02-05 19:03:18 -0500

    Todo CSS

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,14 +2,19 @@ import { TiDeleteOutline } from 'react-icons/ti'
 
 const Task = ({task, onDelete}) => {
     return (
-        <div className = 'task'>
-            <h2>{task.text} 
-            <TiDeleteOutline style={{color: 'red', cursor: 'pointer'}} 
-            onClick ={() => onDelete(task.id)}/> 
-            </h2>
-            
-            <p>{task.date} at {task.time}</p>
+        <div className='TodoDiv'>
+
+            <div className = 'individualTodo'>
+                <h2 className='todoTitle'>{task.text} 
+                <TiDeleteOutline className='cancelTodo' style={{color: '#D23636', cursor: 'pointer'}} 
+                onClick ={() => onDelete(task.id)}/> 
+                </h2>
+                
+                <p className='todoCaption'>{task.date} at {task.time}</p>
+            </div>
+
         </div>
+        
     )
 }
 
