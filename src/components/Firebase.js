258d15f3c7acfde10ca20dfaab52a310850cffee
MihirSeth <mihirseth2005@gmail.com>
2022-02-06 10:50:02 +0530

    commit

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,11 +1,34 @@
 
-import 'firebase/compat/firestore';
-import firebase from 'firebase/compat/app';
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC8plFEg9Ug9wC9DK3GSZOqLTT87ycV61g",
+//   authDomain: "uot-hackathon-d1638.firebaseapp.com",
+//   projectId: "uot-hackathon-d1638",
+//   storageBucket: "uot-hackathon-d1638.appspot.com",
+//   messagingSenderId: "689490705372",
+//   appId: "1:689490705372:web:3641fe479b71bb68292d19",
+//   measurementId: "G-SB0WBFG4JW"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// // const db = app.firestore();
+// const auth = getAuth(app);
+
+// export {auth}
+
+
+
+import {getFirestore} from 'firebase/firestore';
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyC8plFEg9Ug9wC9DK3GSZOqLTT87ycV61g",
@@ -17,31 +40,9 @@ const firebaseConfig = {
   measurementId: "G-SB0WBFG4JW"
 };
 
-const app = initializeApp(firebaseConfig);
-// const db = app.firestore();
-const auth = getAuth(app);
+const firebaseApp = initializeApp(firebaseConfig);
 
-export {auth}
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
 
-
-
-
-
-// import firebase from 'firebase/compat/app';
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-
-// var firebaseApp = firebase.initializeApp({
-//   apiKey: "AIzaSyC8plFEg9Ug9wC9DK3GSZOqLTT87ycV61g",
-//   authDomain: "uot-hackathon-d1638.firebaseapp.com",
-//   projectId: "uot-hackathon-d1638",
-//   storageBucket: "uot-hackathon-d1638.appspot.com",
-//   messagingSenderId: "689490705372",
-//   appId: "1:689490705372:web:3641fe479b71bb68292d19",
-//   measurementId: "G-SB0WBFG4JW"
-// });
-
-// var db = firebaseApp.firestore();
-// const auth = getAuth(firebaseApp);
-
-// export { db,auth };
+export { db,auth };
